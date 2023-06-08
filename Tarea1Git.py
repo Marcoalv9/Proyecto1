e54d5c5cd076439bcfba9402491956d226e4ea93
DonMarco <marco500ag@gmail.com>
2023-06-08 15:51:56 -0600

    Actualización
    se añadieron nuevas peliculas

--- Tarea1Git.py
+++ Tarea1Git.py
@@ -5,6 +5,13 @@ pelicula={"Nombre": ["Super Mario Bros", "The Whale", "Spiderman: Across", "Nott
 "Género":["animada", "drama", "animada", "romance", "anime", "Drama" ],
 "Duración": ["1h32m", "1h57m", "2h20m", "2h04m", "1h42m", "2h10m"],
 "Puntaje Rotten Tomatoes": [60, 65, 96, 85, 99, 77]}
-
 pl= pd.DataFrame(pelicula)
-print(pl)
+nueva_informacion = {
+    "Nombre": ["Titanic", "The Avengers", "Interstellar", "The Shawshank Redemption", "The Dark Knight", "Inception"],
+    "Género": ["romance", "acción", "ciencia ficción", "drama", "acción", "ciencia ficción"],
+    "Duración": ["3h14m", "2h23m", "2h49m", "2h22m", "2h32m", "2h28m"],
+    "Puntaje Rotten Tomatoes": [89, 92, 74, 91, 94, 87]
+}
+nueva_pl = pd.DataFrame(nueva_informacion)
+pl_actualizado = pd.concat([pl, nueva_pl], axis=0).reset_index(drop=True)
+print(pl_actualizado)
