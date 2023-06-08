bf8e054e1373e75b43af9582a3504aa7ebff42b3
DonMarco <marco500ag@gmail.com>
2023-06-08 15:21:03 -0600

    Dataframe
    se crea un Df sobre películas

--- Tarea1Git.py
+++ Tarea1Git.py
@@ -1,2 +1,10 @@
 import pandas as pd 
 import numpy as np
+
+pelicula={"Nombre": ["Super Mario Bros", "The Whale", "Spiderman: Across", "Notting Hill", "Kiki:entregas a domicilio", "Green Book"], 
+"Género":["animada", "drama", "animada", "romance", "anime", "Drama" ],
+"Duración": ["1h32m", "1h57m", "2h20m", "2h04m", "1h42m", "2h10m"],
+"Puntaje Rotten Tomatoes": [60, 65, 96, 85, 99, 77]}
+
+pl= pd.DataFrame(pelicula)
+print(pl)
